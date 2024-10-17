b9e00940b62a374dbba3ee9053a6d669ad58cccb
NPI <nicoilforte36@gmail.com>
2024-10-17 16:12:06 +0200

    update the structure of the project

--- index.py
+++ index.py
@@ -2,19 +2,25 @@ from dotenv import load_dotenv
 import os
 
 from modules.get_unife_schedules import get_semester_from_unife
-from modules.calendar_api import update_calendar, build_service, get_semester_from_calendar
+from modules.calendar_api import update_calendar, build_service, get_semester_from_calendar, get_calendars_info
 
 load_dotenv()
 
 # If modifying these scopes, delete the file token.json.
-calendar_id = os.getenv("CALENDAR_ID")
 
 def main():
     calendar = build_service('calendar')
-
-    unife_schedule = get_semester_from_unife()
-    google_calendar_events = get_semester_from_calendar(calendar, calendar_id)
-    update_calendar(calendar, unife_schedule, google_calendar_events, calendar_id)
+    
+    calendars_info = get_calendars_info(calendar)
+    
+    for info in calendars_info:
+        print("Processing calendar: " + info['name'])
+        calendar_id = info['calendar_id']
+        course_id = info['course_id']
+        unife_schedule = get_semester_from_unife(course_id, info["year2"])
+        google_calendar_events = get_semester_from_calendar(calendar, calendar_id)
+        update_calendar(calendar, unife_schedule, google_calendar_events, calendar_id)
+    
 
 
 
--- modules/get_unife_schedules.py
+++ modules/get_unife_schedules.py
@@ -5,8 +5,8 @@ import os.path
 
 subjects = {"ISTITUZIONI DI MATEMATICA": "1", "PROGRAMMAZIONE E LABORATORIO" : "2", "ECONOMIA E GESTIONE AZIENDALE" : "3", "LINGUA INGLESE: VERIFICA DELLE CONOSCENZE" : "4"}
 
-#url = "https://aule.unife.it/AgendaStudenti/grid_call.php?view=easycourse&form-type=corso&include=corso&txtcurr=1+-+Percorso+Comune&anno=2024&corso=1233&anno2%5B%5D=PDS0%7C1&date=08-10-2024&periodo_didattico=&_lang=en&list=&week_grid_type=-1&ar_codes_=&ar_select_=&col_cells=0&empty_box=0&only_grid=0&highlighted_date=0&all_events=0&faculty_group=0&_lang=en&all_events=0&txtcurr=1+-+Percorso+Comune"
-def get_week(req_date):
+
+def get_week(req_date, id_course, year2):
     url = "https://aule.unife.it/AgendaStudenti/grid_call.php"
     payload = {
         "view": "easycourse",
@@ -14,8 +14,8 @@ def get_week(req_date):
         "include": "corso",
         "txtcurr": "1 - Percorso Comune",
         "anno": "2024",
-        "corso": "1233",
-        "anno2[]": "PDS0|1",
+        "corso": id_course,
+        "anno2[]": year2,
         "date": req_date,
         "periodo_didattico": "",
         "_lang": "en",
@@ -34,9 +34,13 @@ def get_week(req_date):
         "txtcurr": "1 - Percorso Comune"
     }
 
+
     events_list = []
 
     response = requests.get(url, params=payload)
+    
+    with open('response.json', 'w') as f:
+        json.dump(response.json(), f)
 
     #write in a json file the response in a json econded format
     lessons = response.json()['celle']
@@ -59,7 +63,7 @@ def get_week(req_date):
             'summary': f"{lesson['nome_insegnamento']} - {lesson['tipo']}",
             'description': lesson['docente'],
             'location': lesson['aula'],
-            "colorId": subjects[lesson['nome_insegnamento']],
+            "colorId": '1',
             'start': {
                 'dateTime': start,
                 'timeZone': 'Europe/Rome',
@@ -73,13 +77,15 @@ def get_week(req_date):
         events_list.append(event)
     return events_list
 
-def get_semester_from_unife():
-
+def get_semester_from_unife(id_course, year2):
     curr_date = date.today()
+    
     unife_schedule = []
+    
     semester_end_date = date(int(os.getenv("ANNOSEMESTRE1")), 12, 31) if date.today() < date(int(os.getenv("ANNOSEMESTRE1")), 12, 31) else date(int(os.getenv("ANNOSEMESTRE2")), 5, 30)
+    
     while curr_date < semester_end_date:
-        print(curr_date)
-        unife_schedule += get_week(curr_date.strftime("%d-%m-%Y"))
+        unife_schedule += get_week(curr_date.strftime("%d-%m-%Y"), id_course, year2)
         curr_date += timedelta(days=7)
+        
     return unife_schedule
